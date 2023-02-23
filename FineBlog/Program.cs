a4935320bb3462e1971564d0ba091e8be9b92041
halilcan turan <c.turan0405@gmail.com>
2023-02-23 18:33:51 +0300

    login theme added

--- FineBlog/Program.cs
+++ FineBlog/Program.cs
@@ -1,15 +1,30 @@
+using FineBlog.Data;
+using FineBlog.Models;
+using FineBlog.Utilites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IDbInitializer,DbInitializer>();
+
 var app = builder.Build();
+DataSeeding();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -21,7 +36,21 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
 app.Run();
+
+void DataSeeding()
+{
+    using(var scope = app.Services.CreateScope())
+    {
+        var DbInitialize = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        DbInitialize.Initialize();
+    }
+}
---
+++ FineBlog/Utilites/WebsiteRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace FineBlog.Utilites
+{
+    public class WebsiteRoles
+    {
+        public const string? WebsiteAdmin = "Admin";
+        public const string? WebsiteAuthor = "Author";
+
+    }
+}
