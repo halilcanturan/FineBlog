f998c2d935ac2eafafeed8046378d9c2def37986
halilcan turan <c.turan0405@gmail.com>
2023-02-27 19:23:29 +0300

    post ekleme fonksiyonu

--- FineBlog/Areas/Admin/Controllers/PostController.cs
+++ FineBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,6 +1,11 @@
-﻿using FineBlog.ViewModel;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using FineBlog.Data;
+using FineBlog.Models;
+using FineBlog.ViewModel;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.CodeAnalysis;
 
 namespace FineBlog.Areas.Admin.Controllers
@@ -9,6 +14,22 @@ namespace FineBlog.Areas.Admin.Controllers
     [Authorize]
     public class PostController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public PostController(ApplicationDbContext context,
+                                INotyfService notyfService,
+                                IWebHostEnvironment webHostEnvironment,
+                                UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _notification = notyfService;
+            _webHostEnvironment = webHostEnvironment;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -21,6 +42,52 @@ namespace FineBlog.Areas.Admin.Controllers
             return View(new CreatePostVM());
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePostVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
 
+            //get logged in user id
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x=>x.UserName == User.Identity!.Name );
+
+
+            var post = new Post();
+            
+            post.Title= vm.Title;
+            post.Description = vm.Description;
+            post.ShortDescription=vm.ShortDescription;
+            post.ApplicationUserId = loggedInUser!.Id;
+
+            if(post.Title!= null)
+            {
+                string slug = vm.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid(); 
+            }
+
+            if(vm.Thumbnail != null)
+            {
+                post.ThumbnailUrl=UploadImage(vm.Thumbnail);
+            }
+
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync();
+            _notification.Success("Post Created Succesfully");
+            return RedirectToAction("Index");
+             
+        }
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath= Path.Combine(folderPath, uniqueFileName);
+            using(FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
+
+        }
     }
 }
