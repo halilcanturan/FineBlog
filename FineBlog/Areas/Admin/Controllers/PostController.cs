39ce4ee016eaf7370ec679f32e854f51f93c9099
halilcan turan <c.turan0405@gmail.com>
2023-02-27 23:28:30 +0300

    delete post function

--- FineBlog/Areas/Admin/Controllers/PostController.cs
+++ FineBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,6 +1,7 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using FineBlog.Data;
 using FineBlog.Models;
+using FineBlog.Utilites;
 using FineBlog.ViewModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -30,9 +31,30 @@ namespace FineBlog.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var listOfPosts = new List<Post>();
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+            if (loggedInUserRole[0] ==WebsiteRoles.WebsiteAdmin)
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).ToListAsync();
+            }
+            else
+            {
+                listOfPosts = await _context.Posts!.Include(x => x.ApplicationUser).Where(x=>x.ApplicationUser!.Id==loggedInUser!.Id).ToListAsync();
+            }
+            var listofPostsVM = listOfPosts.Select(x => new PostVM()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                CreatedDate = x.CreatedDate,
+                ThumbnailUrl = x.ThumbnailUrl,
+                AuthorName = x.ApplicationUser.FirstNamer + " " + x.ApplicationUser.LastName,
+            }).ToList();
+            return View(listofPostsVM);
         }
 
 
@@ -73,9 +95,27 @@ namespace FineBlog.Areas.Admin.Controllers
             await _context.Posts!.AddAsync(post);
             await _context.SaveChangesAsync();
             _notification.Success("Post Created Succesfully");
-            return RedirectToAction("Index");
-             
+            return RedirectToAction("Index");             
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var post = await _context.Posts!.FirstOrDefaultAsync(x=>x.Id == id);
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin || loggedInUser?.Id == post?.ApplicationUserId)
+            {
+                 _context.Posts!.Remove(post!);
+                await _context.SaveChangesAsync();
+                _notification.Success("Post Deleted Successfully");
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+            return View();
+        }
+
         private string UploadImage(IFormFile file)
         {
             string uniqueFileName = "";
