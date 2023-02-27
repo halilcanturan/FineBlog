cad903970558aa1ce322ed571350f97020b59828
halilcan turan <c.turan0405@gmail.com>
2023-02-27 18:26:41 +0300

    post create view

--- FineBlog/Areas/Admin/Controllers/PostController.cs
+++ FineBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FineBlog.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FineBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class PostController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreatePostVM());
+        }
+
+
     }
 }
